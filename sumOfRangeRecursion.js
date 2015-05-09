22b9f29022a15d6dddc7b1989861ff2d4210d4d5
LauraKirby <laurakirby26@gmail.com>
2015-05-08 22:32:32 -0700

    set up jasmine and solution to sumOfRange using recursion

---
+++ sumOfRangeRecursion.js
@@ -0,0 +1,22 @@
+//Define a function sumOfRange that takes a number x and 
+//returns the sum of all digits between 0 and x.
+
+//Iterate to find solution
+// function sumOfRange(x){
+// 	var rangeItem = 0; 
+// 	for (var i = 0; i < x; i++){
+// 		rangeItem += i; 
+// 	}
+//   return rangeItem; 
+// }
+// console.log(sumOfRange(10)); 
+
+//use recursion to find solution 
+function sumOfRangeR (x){
+if (n === 0) {
+	return 1; 
+}
+return (x + sumOfRangeR(x-1));
+
+}
+sumOfRange(10);
